a9765b617a2423da264c1b86b422db0d43382f81
Ryan Boesch <ryanboesch@gmail.com>
2016-03-26 03:30:57 -0700

    channel plots

--- thesis.m
+++ thesis.m
@@ -1,7 +1,7 @@
 addpath('~/thesis/matlab/lib');
 addpath('/cad/cadence/MMSIM10.11.128.lnx86/tools.lnx86/spectre/matlab/64bit');
 stanford_red = [140, 21, 21]/255;
-new_blue = [0, 0.447 0.741];
+new_blue = [0, 0.447, 0.741];
 font_size = 18;
 font_size_label = 24;
 
